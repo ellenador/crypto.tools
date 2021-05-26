ccbdd70040ef30f340a53016c09cff6429d4d42d
ellenador <feliped7@protonmail.com>
2021-05-26 06:53:10 -0500

    proyecto modificado

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,88 +1,16 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-app-bar dense :clipped-left="clipped" fixed app>
+      <v-toolbar-title to="/" v-text="title" />
+      <v-btn to="/" small text class="ml-9">Home</v-btn>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -90,28 +18,20 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Crypto.tools",
+    };
+  },
+};
 </script>
+
+<style scoped>
+.theme--dark .v-app-bar,
+.v-navigation-drawer,
+.v-list-item,
+.v-footer {
+  border: 1px rgb(170, 170, 170) !important;
+  border-style: dashed double !important;
+}
+</style>
---
+++ pages/parachains.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <h1 class="mt-4">Parachains of Kusama</h1>
+
+    <v-simple-table dense class="mt-4">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Names</th>
+            <th class="text-left">Symbol</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Link</th>
+            <th class="text-left">Rewards</th>
+          </tr>
+        </thead>
+        <tbody class="grey--text mb-2">
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.symbol }}</td>
+            <td>{{ item.type }}</td>
+            <td>{{ item.link }}</td>
+            <td>{{ item.rewards }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      desserts: [
+        {
+          name: "Karura",
+          link: "https://acala.network/karura",
+          symbol: "KAR",
+          type: "DeFi",
+          rewards: "1 KSM = 12 KAR",
+        },
+        {
+          name: "Moonriver",
+          link: "https://moonbeam.network/networks/moonriver/",
+          symbol: "RIVER",
+          type: "Smart Contract",
+          rewards: "",
+        },
+        {
+          name: "Shiden Network",
+          link: "https://shiden.plasmnet.io/",
+          symbol: "SDN",
+          type: "Smart Contract",
+          rewards: "1 KSM = 10-20",
+        },
+        {
+          name: "SherpaX",
+          link: 'https://chainx.org/en/',
+          symbol: "Ksx",
+          type: "Interoperability",
+          rewards: "",
+        },
+        {
+          name: "Robonomics",
+          link: 'https://robonomics.network/',
+          symbol: "XRT",
+          type: "Marketplace",
+          rewards: "",
+        },
+        {
+          name: "Bifrost",
+          link: "https://ksm.vtoken.io/ (Code:QN2463)",
+          symbol: "BCN",
+          type: "Staking Protocol",
+          rewards: "1 KSM = 1 svKSM",
+        },
+        {
+          name: "Genshiro",
+          link: 'https://genshiro.equilibrium.io/en',
+          symbol: "GENS",
+          type: "DeFi",
+          rewards: "",
+        },
+        {
+          name: "Subsocial",
+          link: 'https://subsocial.network/',
+          symbol: "SNM",
+          type: "Social Network",
+          rewards: "",
+        },
+        {
+          name: "Darwinia Crab",
+          link: "https://crab.network/",
+          symbol: "CRING",
+          type: "Interoperability",
+          rewards: "",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.v-data-table > .v-data-table__wrapper > table {
+  width: 100%;
+  border-spacing: 0;
+  border: 1px rgb(230, 180, 230) !important;
+  border-style: dashed double !important;
+}
+
+.theme--dark.v-data-table
+  > .v-data-table__wrapper
+  > table
+  > thead
+  > tr:last-child
+  > th {
+  border-bottom: 1px dashed rgb(230, 180, 230) !important;
+}
+
+.theme--dark.v-data-table
+  > .v-data-table__wrapper
+  > table
+  > tbody
+  > tr:hover:not(.v-data-table__expanded__content):not(.v-data-table__empty-wrapper) {
+  background: #cacaca13 !important;
+}
+</style>
+<style scoped>
+.theme--dark, .v-btn {
+  border: 1px rgb(230, 180, 230) !important;
+  border-style: dashed double !important;
+}
+</style>
